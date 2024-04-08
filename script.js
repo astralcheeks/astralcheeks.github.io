39786a508f2c83e6998d2ba178c604e11dbf491b
astralcheeks <adrian.kahali@hotmail.com>
2024-04-08 11:31:36 -0400

    More site updates
    Added multiple sections to site, played around with css a bit more, trying to get the hang of things

--- script.js
+++
@@ -1,7 +0,0 @@
-function revealContact() {
-    document.getElementById("contact").style.display = 'block'
-}
-
-function revealResume() {
-    document.getElementById("resume").style.display = 'block';
-}
